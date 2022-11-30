a465f26fbd7cc1a9c596d28c3d8acf6c944dddde
Andrey <kolsergienko@gmail.com>
2022-11-30 17:16:58 +0300

    add webpack starter

---
+++ src/core/Component/Component.js
@@ -0,0 +1,40 @@
+export class Component extends HTMLElement {
+	constructor() {
+		super();
+		this.state = {};
+		this.props = {};
+	}
+
+	setState(callback) {
+		this.state = callback(this.state);
+		this.innerHTML = this.render()
+	}
+
+	connectedCallback() {
+		this.innerHTML = this.render()
+		this.componentDidMount()
+	}
+
+	disconnectedCallback() {
+		this.componentWillUnmount();
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.componentWillUpdate(name, oldValue, newValue);
+		this.props[name] = this.getAttribute(name);
+	}
+
+	dispatch(type, props) {
+		this.dispatchEvent(
+			new CustomEvent(type, { detail: props, bubbles: true })
+		);
+	}
+
+	componentWillUpdate() { }
+
+	render() { }
+
+	componentWillUnmount() { }
+
+	componentDidMount() { }
+}
