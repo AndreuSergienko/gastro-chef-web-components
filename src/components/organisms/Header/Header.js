69beebe7c4a3f8f660a3e5e02ab0514d5780fc31
Andrey <kolsergienko@gmail.com>
2022-12-02 01:24:17 +0300

    add Header Component with its functionality n styls

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -1,1 +1,71 @@
-import './Header.scss'
+import './Header.scss';
+import { Component } from '../../../core';
+import '../../atoms';
+import '../../molecules';
+import { EVENTS } from '../../constants';
+
+export class Header extends Component {
+   constructor() {
+      super();
+      this.state = {
+         navItems: [
+            {
+               href: '#',
+               label: 'Программы питания',
+            },
+            {
+               href: '#',
+               label: 'Бизнес-ланчи',
+            },
+            {
+               href: '#',
+               label: 'Gastro Shop',
+            },
+            {
+               href: '#',
+               label: 'О нас',
+            },
+            {
+               href: '#',
+               label: 'Блог',
+            },
+         ]
+      }
+   }
+
+   render() {
+      return `
+         <header class="header">
+            <div class="container">
+               <div class="header-container">
+                  <gastro-logo 
+                     href="#" 
+                     classname="header" 
+                     src="header/logo.png"
+                  >
+                  </gastro-logo>
+                  <gastro-navigation 
+                     items='${JSON.stringify(this.state.navItems)}'
+                     classname="header"
+                  >
+                  </gastro-navigation>
+                  <gastro-language 
+                     classname="header"
+                  >
+                  </gastro-language>
+                  <gastro-phone hasphoneicon="true" classname="header">
+                  </gastro-phone>
+                  <gastro-button 
+                     classname="header__burger-button action-button burger-button" 
+                     src="header/burger.svg"
+                     evttype="${EVENTS.openMenu}"
+                  >
+                  </gastro-button>
+               </div>
+            </div>
+         </header>
+      `;
+   }
+}
+
+customElements.define('gastro-header', Header)
