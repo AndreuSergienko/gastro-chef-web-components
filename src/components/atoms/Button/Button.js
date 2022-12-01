6176df3f784b400520a61347880470668db264a6
Andrey <kolsergienko@gmail.com>
2022-12-02 01:20:01 +0300

    add Button with its functionality

---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,44 @@
+import './Button.scss'
+import { Component } from '../../../core'
+
+export class Button extends Component {
+   constructor() {
+      super()
+   }
+
+   onClick(evt) {
+      if (evt.target.closest('.burger-button')) {
+         this.dispatch(this.props.evttype)
+      }
+      if (evt.target.closest('.close-burger')) {
+         this.dispatch(this.props.evttype)
+      }
+   }
+
+   componentDidMount() {
+      this.addEventListener('click', this.onClick)
+   }
+
+   static get observedAttributes() {
+      return ['src', 'content', 'classname', 'evttype']
+   }
+
+   render() {
+      return `
+      <button class="${this.props.classname}">
+         ${this.props.src ?
+            `<img src="../../../assets/images/${this.props.src}" alt="Button" />`
+            :
+            ''
+         }
+         ${this.props.content ?
+            `${this.props.content}`
+            :
+            ''
+         }
+      </button>
+      `
+   }
+}
+
+customElements.define('gastro-button', Button)
