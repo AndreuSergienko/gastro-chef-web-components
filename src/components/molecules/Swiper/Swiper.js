60169d6c88f78e66b595ca06f4776c7fcd753547
Andr.dre <kolsergienko@gmail.com>
2022-12-03 21:09:43 +0300

    add some formating for Swiper

--- src/components/molecules/Swiper/Swiper.js
+++ src/components/molecules/Swiper/Swiper.js
@@ -11,44 +11,44 @@ export class MySwiper extends Component {
 		super();
 	}
 
-	initSwiper() {
-		Swiper.use([EffectFade, Autoplay]);
+    initSwiper() {
+        Swiper.use([EffectFade, Autoplay]);
 
-		const swiper = new Swiper(`.${this.props.swiperclassname}`, {
-			modules: [Navigation, Pagination],
-			loop: true,
-			effect: 'fade',
-			allowTouchMove: false,
-			autoplay: {
-				delay: 4000,
-				disableOnInteraction: false,
-			},
+        const swiper = new Swiper(`.${this.props.swiperclassname}`, {
+            modules: [Navigation, Pagination],
+            loop: true,
+            effect: 'fade',
+            allowTouchMove: false,
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+        
+            fadeEffect: {
+                crossFade: true,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                320: {
+                    speed: 700
+                },
+                1200: {
+                    speed: 600
+                }
+            }
+        });        
+    }
 
-			fadeEffect: {
-				crossFade: true,
-			},
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			},
-			breakpoints: {
-				320: {
-					speed: 700,
-				},
-				1200: {
-					speed: 600,
-				},
-			},
-		});
-	}
-
-	componentDidMount() {
-		this.initSwiper();
-	}
+    componentDidMount() {
+        this.initSwiper()
+    }
 
 	static get observedAttributes() {
 		return ['slides', 'swiperclassname'];
@@ -58,68 +58,51 @@ export class MySwiper extends Component {
 		return `
         <div class="swiper ${this.props.swiperclassname}">
             <div class="swiper-wrapper">
-                ${JSON.parse(this.props.slides)
-									.map(
-										(slide, index) =>
-											`
+                ${JSON.parse(this.props.slides).map(
+							(slide, index) =>
+								`
                     <div class="swiper-slide">
-                        <div class="${
-													this.props.swiperclassname
-												}-slide slide-0${index + 1}">
+                        <div class="${this.props.swiperclassname}-slide slide-0${index + 1}">
                             <div class="container slide-container">
-                                <div class="${
-																	this.props.swiperclassname
-																}-slide__info">
-                                    <h3 class="${
-																			this.props.swiperclassname
-																		}-slide__info-title">
+                                <div class="${this.props.swiperclassname}-slide__info">
+                                    <h3 class="${this.props.swiperclassname}-slide__info-title">
                                         ${slide.title}
                                     </h3>
-                                    ${
-																			slide.subtitle
-																				? `
+                                    ${slide.subtitle ? 
+                                        `
                                         <p class="${this.props.swiperclassname}-slide__info-subtitle">                
                                             ${slide.subtitle}
                                         </p>
                                         `
-																				: ''
-																		}
-                                    <div class="${
-																			this.props.swiperclassname
-																		}-slide__info-bottom">
+                                        :
+                                        ''
+									}
+                                    <div class="${this.props.swiperclassname}-slide__info-bottom">
                                         <gastro-button
-                                            classname="${
-																							this.props.swiperclassname
-																						}-slide__info-btn primary-button"
+                                            classname="${this.props.swiperclassname}-slide__info-btn primary-button"
                                             content="${slide.btncontent}"
                                         >
                                         </gastro-button>
-                                        <p class="${
-																					this.props.swiperclassname
-																				}-slide__info-test">
+                                        <p class="${this.props.swiperclassname}-slide__info-test">
                                             ${slide.subinfo.text}
                                             ${
-																							slide.subinfo.price
-																								? `<span class="${this.props.swiperclassname}-slide__info-test-price">${slide.subinfo.price}</span>`
-																								: ''
-																						}
+											    slide.subinfo.price	?
+                                                `<span class="${this.props.swiperclassname}-slide__info-test-price">${slide.subinfo.price}</span>`
+													: 
+                                                ''
+											}
                                         </p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="${
-															this.props.swiperclassname
-														}-slide__circle-img circle-img-bg-${index + 1}">
-                                <img src="../../../assets/images/slides/${
-																	slide.src
-																}" alt="Slide" />
+                            <div class="${this.props.swiperclassname}-slide__circle-img circle-img-bg-${index + 1}">
+                                <img src="../../../assets/images/slides/${slide.src}" alt="Slide" />
                             </div>
                         </div>
                     </div>            
                             `
-									)
-									.join('')}
+				).join('')}
             </div>
         
             <div class="swiper-pagination"></div>
