1987fb51d80284c448a6d1175b1931dfec10f836
Andr.dre <kolsergienko@gmail.com>
2022-12-03 18:02:15 +0300

    remove extra import

--- src/components/molecules/Swiper/Swiper.js
+++ src/components/molecules/Swiper/Swiper.js
@@ -5,7 +5,6 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
-import './Swiper.scss';
 
 export class MySwiper extends Component {
 	constructor() {
