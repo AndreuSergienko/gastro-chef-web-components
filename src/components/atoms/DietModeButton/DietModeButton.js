0adefa0cdc823f1efd78f1415147d8697ee077aa
Andr.dre <kolsergienko@gmail.com>
2022-12-05 19:43:07 +0300

    add evt dispatch

--- src/components/atoms/DietModeButton/DietModeButton.js
+++ src/components/atoms/DietModeButton/DietModeButton.js
@@ -7,19 +7,31 @@ export class DietModeButton extends Component {
   }
 
   static get observedAttributes() {
-    return ['mode']
+    return ['mode', 'isactive', 'index']
+  }
+
+  onClick() {
+    this.dispatch('switch-mode', {
+      mode: this.props.mode,
+      clickedButtonIndex: +this.props.index,
+    })
+  }
+
+  componentDidMount() {
+    this.addEventListener('click', this.onClick)
   }
 
   render() {
-    this.props.mode = JSON.parse(this.props.mode)
-    const { mode } = this.props;
+    this.props.mode = JSON.parse(this.props.mode);
+    this.props.isactive = JSON.parse(this.props.isactive)
+    const { mode, isactive } = this.props;
     return `
-      <button class="diet__modes-btn">
+      <button class="diet__modes-btn ${isactive ? 'active' : ''}">
         <span class="diet__modes-btn-title">${mode.title}</span>
         <span class="diet__modes-btn-cal">${mode.calories} ккал</span>
       </button>
     `
   }
-}
+};
 
 customElements.define('gastro-diet-mode-button', DietModeButton);
