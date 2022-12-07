1b9587ba3a1a6bab330a1a917245c4a110de3991
Andr.dre <kolsergienko@gmail.com>
2022-12-07 22:18:51 +0300

    remove extra div

--- src/components/molecules/HeroSwiper/HeroSwiper.js
+++ src/components/molecules/HeroSwiper/HeroSwiper.js
@@ -60,54 +60,51 @@ export class HeroSwiper extends Component {
          <div class="swiper ${this.props.swiperclassname}">
             <div class="swiper-wrapper">
                ${JSON.parse(this.props.slides).map((slide) => (`		
-                  <div class="swiper-slide">
-                     <div class="${this.props.swiperclassname}-slide">
-                        <div class="container slide-container">
-                           <div class="${this.props.swiperclassname}-slide__info">
-                              <h3
-                                 class="${this.props.swiperclassname}-slide__info-title"
-                              >
-                                 ${slide.title}
-                              </h3>
-                              ${slide.subtitle ? `
-                              <p
-                                 class="${this.props.swiperclassname}-slide__info-subtitle"
-                              >                
-                                 ${slide.subtitle}
-                              </p>` : ''}
-                              <div
-                                 class="${this.props.swiperclassname}-slide__info-bottom"
-                              >
-                                 <gastro-button
-                                    classname="${this.props.swiperclassname}-slide__info-btn primary-button"
-                                    content="${slide.btncontent}"
-                                 >
-                                 </gastro-button>
-                                 <p
-                                    class="${this.props.swiperclassname}-slide__info-test"
-                                 >
-                                    ${slide.subinfo.text}
-                                    ${slide.subinfo.price ? `
-                                    <span 
-                                       class="${this.props.swiperclassname}-slide__info-test-price"
-                                    >
-                                       ${slide.subinfo.price}
-                                    </span>`: ''}
-                                 </p>
-                              </div>
-                           </div>
-                        </div>
-
-                        <div
-                           class="${this.props.swiperclassname}-slide__circle-img"
+               <div class="swiper-slide ${this.props.swiperclassname}-slide">
+                  <div class="container slide-container">
+                     <div class="${this.props.swiperclassname}-slide__info">
+                        <h3
+                           class="${this.props.swiperclassname}-slide__info-title"
                         >
-                           <img
-                              src="../../../assets/images/slides/${slide.src}" alt="Slide" 
-                           />
+                           ${slide.title}
+                        </h3>
+                        ${slide.subtitle ? `
+                        <p
+                           class="${this.props.swiperclassname}-slide__info-subtitle"
+                        >                
+                           ${slide.subtitle}
+                        </p>` : ''}
+                        <div
+                           class="${this.props.swiperclassname}-slide__info-bottom"
+                        >
+                           <gastro-button
+                              classname="${this.props.swiperclassname}-slide__info-btn primary-button"
+                              content="${slide.btncontent}"
+                           >
+                           </gastro-button>
+                           <p
+                              class="${this.props.swiperclassname}-slide__info-test"
+                           >
+                              ${slide.subinfo.text}
+                              ${slide.subinfo.price ? `
+                              <span 
+                                 class="${this.props.swiperclassname}-slide__info-test-price"
+                              >
+                                 ${slide.subinfo.price}
+                              </span>`: ''}
+                           </p>
                         </div>
                      </div>
-                  </div>`))
-            .join('')}
+                  </div>
+
+                  <div
+                     class="${this.props.swiperclassname}-slide__circle-img"
+                  >
+                     <img
+                        src="../../../assets/images/slides/${slide.src}" alt="Slide" 
+                     />
+                  </div>
+               </div>`)).join('')}
             </div>
         
             <div class="swiper-pagination"></div>
