357d1e60ca3cba852e7ca0447fd009ad523396f4
Andr.dre <kolsergienko@gmail.com>
2022-12-08 21:47:47 +0300

    add initial state fields

---
+++ src/components/molecules/AssistanceOrder/initialState.js
@@ -0,0 +1,14 @@
+export const initialFieldsState = {
+    username: {
+        value: "",
+        errors: {},
+        isTouched: false,
+        isValid: false,
+    },
+    usertel: {
+        value: "",
+        errors: {},
+        isTouched: false,
+        isValid: false,
+    },
+};
