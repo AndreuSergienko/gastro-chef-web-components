e6496940325432c14ecc470cc97ef82d9773cafb
Andr.dre <kolsergienko@gmail.com>
2022-12-09 15:38:27 +0300

    add open question mechanism

--- src/components/molecules/AssistanceFaq/AssistanceFaq.js
+++ src/components/molecules/AssistanceFaq/AssistanceFaq.js
@@ -1,179 +1,47 @@
 import './AssistanceFaq.scss';
 import { Component } from '../../../core';
+import '../../atoms/AssistanceFaqQuestion'
 
 export class AssistanceFaq extends Component {
     constructor() {
-        super()
+        super();
+        this.state = {
+            activeQuestionIndex: null,
+        }
+    }
+
+    static get observedAttributes() {
+        return ['items']
+    }
+
+    onOpen({ detail }) {
+        this.setState((state) => ({
+            ...state,
+            activeQuestionIndex:
+                state.activeQuestionIndex === detail.activeIndex ? null : detail.activeIndex
+        }))
+    }
+
+    componentDidMount() {
+        this.addEventListener('open-question', this.onOpen)
     }
 
     render() {
         return `
         <div class="assistance__faq">
-        <h3 class="assistance__faq-title">Часто задаваемые вопросы</h3>
-        <div class="assistance__faq-questions">
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        Как осуществляется доставка правильного питания?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span> Lorem. </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        Можно ли менять время доставки и место?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span> Lorem, ipsum. </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        Как и в чем приезжает еда?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span> Lorem, ipsum dolor. </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        Когда Вы готовите?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span> Lorem ipsum dolor sit. </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        Какие продукты Вы используете?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span> Lorem ipsum dolor sit amet. </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        Я буду есть одно и то же?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span> Lorem ipsum dolor sit amet consectetur. </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        У меня аллергия на опредёленные продукты
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span>
-                            Lorem ipsum dolor sit amet consectetur adipisicing.
-                        </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        В какой очередности все есть?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit.
-                        </span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="assistance__faq-question">
-                <div class="assistance__faq-question-btn-group">
-                    <span class="assistance__faq-question-text">
-                        Можно ли замораживать программу?
-                    </span>
-                    <button class="assistance__faq-question-open-btn">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                <div class="assistance__faq-question-answer">
-                    <div class="assistance__faq-question-answer-item">
-                        <span>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Cumque.
-                        </span>
-                    </div>
-                </div>
+            <h3 class="assistance__faq-title">Часто задаваемые вопросы</h3>
+            <div class="assistance__faq-questions">
+            ${JSON.parse(this.props.items).map(({ question, answer }, index) => (`
+                <gastro-faq-question
+                    question="${question}"
+                    answer="${answer}"
+                    index="${index}"
+                    is-open="${index === this.state.activeQuestionIndex}"
+                >
+                </gastro-faq-question>
+            `)).join('')}
             </div>
         </div>
-    </div>
         `
     }
 }
