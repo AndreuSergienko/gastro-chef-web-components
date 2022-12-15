c90a82ca2a929de746ca5112943e0ac7ccb8b4fd
Andr.dre <kolsergienko@gmail.com>
2022-12-15 22:34:02 +0300

    add blog page

---
+++ src/components/pages/Blog/Blog.js
@@ -0,0 +1,72 @@
+import { Component } from "../../../core";
+import '../../organisms/Sections'
+import { EVENTS } from "../../../constants";
+
+export class BlogPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            articlesItems: [
+                {
+                    poster: 'article-01.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-02.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-03.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-01.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-02.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-03.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-01.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-02.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+                {
+                    poster: 'article-03.png',
+                    title: 'Как поменять свои пищевые привычки?',
+                    date: '15.12.2022',
+                },
+            ],
+            headerModificator: 'circle-bg',
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return `
+        <gastro-articles items='${JSON.stringify(this.state.articlesItems)}'>
+        </gastro-articles>
+    `;
+    }
+}
+
+customElements.define('gastro-blog-page', BlogPage)
