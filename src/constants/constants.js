696d4ea7bb0d5270d66b598bcb478bc802220623
Andr.dre <kolsergienko@gmail.com>
2022-12-15 22:26:00 +0300

    add app routes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,4 +2,12 @@ export const EVENTS = {
    openMenu: 'open-menu',
    closeMenu: 'close-menu',
    switchMode: 'switch-mode',
+   addModificator: 'add-modificator',
+}
+
+export const APP_ROUTES = {
+   homePage: '/',
+   blogPage: '/blog',
+   aboutPage: '/about',
+   errorPage: '*',
 }
