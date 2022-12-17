bde60634b68464b2dc77de1a021c1d8b57e873c9
Andr.dre <kolsergienko@gmail.com>
2022-12-18 02:02:45 +0300

    add admin page and styles

---
+++ src/components/pages/Admin/Admin.js
@@ -0,0 +1,95 @@
+import './AdminPanel.scss';
+import { Component, FormManager } from "../../../core";
+import { storageService, articleService } from "../../../services";
+import '../../atoms/Preloader';
+
+
+export class AdminPage extends Component {
+   constructor() {
+      super();
+      this.state = {
+         isLoading: false,
+      }
+      this.form = new FormManager();
+   }
+
+   createArticle = (data) => {
+      this.toggleIsLoading()
+      storageService
+         .uploadPoster(data.poster)
+         .then((snapshot) => {
+            storageService.getDownloadURL(snapshot.ref).then((url) => {
+               articleService
+                  .addArticle({
+                     ...data,
+                     poster: url,
+                  })
+                  .catch((error) => {
+                     console.log(error);
+                  })
+                  .finally(() => {
+                     this.toggleIsLoading()
+                  })
+            });
+         })
+   };
+
+   toggleIsLoading() {
+      this.setState((state) => {
+         return {
+            ...state,
+            isLoading: !state.isLoading,
+         };
+      });
+   }
+
+   componentDidMount() {
+      this.addEventListener("submit", this.form.handleSubmit(this.createArticle));
+   }
+
+   render() {
+      return `
+         <gastro-preloader is-loading="${this.state.isLoading}">
+            <div class="admin-panel">
+               <div class="container">
+                  <div class="admin-panel-container">
+                     <form id="send-data">
+
+                        <div class="mb-3">
+                           <label class="form-label">Название блога</label>
+                           <input class="form-control" type="text" name="title">
+                        </div>
+
+                        <div class="mb-3">
+                           <label class="form-label">Загрузить обложку</label>
+                           <input class="form-control" type="file" id="formFile" name="poster">
+                        </div>
+
+                        <div class="mb-3">
+                           <label
+                              for="exampleFormControlTextarea1" 
+                              class="form-label"
+                           >
+                              Содержание статьи
+                           </label>
+                           <textarea
+                              name="description" 
+                              class="form-control"
+                              id="exampleFormControlTextarea1"
+                              rows="3"
+                           >
+                           </textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                           Send
+                        </button>
+                     </form>
+                  </div>   
+               </div>
+            </div>
+         </gastro-preloader>
+    `;
+   }
+}
+
+customElements.define("gastro-admin-page", AdminPage);
