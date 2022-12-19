3ab2e7cb02c3a3c9b3c73c211bfa97705797060a
Andr.dre <kolsergienko@gmail.com>
2022-12-19 22:48:40 +0300

    add Overlay component

---
+++ src/components/atoms/Overlay/Overlay.js
@@ -0,0 +1,65 @@
+import { Component } from "../../../core";
+
+export class Overlay extends Component {
+    constructor() {
+        super();
+        this.isShadow = true;
+    }
+
+    static get observedAttributes() {
+        return ["is-open"];
+    }
+
+    addClass() {
+        if (JSON.parse(this.props["is-open"])) {
+            this.classList.add('overlay')
+        } else {
+            this.classList.remove('overlay')
+        }
+    }
+
+    componentDidMount() {
+        this.addClass()
+    }
+
+    render() {
+        return `
+    <style>
+        @keyframes overlay {
+            0% {
+                opacity: 0;
+                visibility: hidden;
+                top: -100%;
+            }
+            100% {
+                opacity: 1;
+                visibility: visible;
+                top: 0;
+            }
+        }
+
+        .overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            width: 100%;
+            background: rgba(2, 2, 2, 0.85);
+            z-index: 100;
+            visibility: hidden;
+            animation: overlay 0.3s ease forwards;
+        }
+    </style>
+
+        <div>
+            ${JSON.parse(this.props["is-open"]) ? `
+            <div class="overlay"></div> ` : ""
+            }
+            <slot></slot
+        <div>    
+    `;
+    }
+}
+
+customElements.define('gastro-overlay', Overlay);
