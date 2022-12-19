377fe980e76bac0babdbd77b749bd58264e7acac
Andr.dre <kolsergienko@gmail.com>
2022-12-19 22:51:22 +0300

    add initial form state for validation

---
+++ src/components/pages/SignIn/initialState.js
@@ -0,0 +1,16 @@
+export const initialFieldsState = {
+  email: {
+    value: "",
+    errors: {},
+    success: 'А ты хорош!',
+    isTouched: false,
+    isValid: false,
+  },
+  password: {
+    value: "",
+    errors: {},
+    success: 'Спорим от 1 до 8 =)',
+    isTouched: false,
+    isValid: false,
+  },
+};
